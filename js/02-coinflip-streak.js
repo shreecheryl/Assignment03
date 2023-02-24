ec68c7637d843ea308a366910f7ff1c960f4463f
shreecheryl <shreecheryl@att.net>
2023-02-24 10:19:24 -0800

    Coin Flip Streak completed

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,9 @@
+let coinFlip;
+do {
+   coinFlip = Math.round(Math.random());
+   if (coinFlip === 1) {
+        console.log("Heads");
+   } else {
+        console.log("Tails")
+   }
+} while (coinFlip === 1);
